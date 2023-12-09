385ecf79245f75e283ad34ed7f75356769debdb9
DESKTOP-IS2G159\kevin <kevin.rodfa@gmail.com>
2023-12-09 03:58:47 -0300

    codigo de api listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import Header from './components/Header'
 import Carrusel from './components/Carrusel'
-import Main from './components/Main'
 import Footer from './components/footer'
 import MiApi from './components/MiApi'
 
@@ -9,10 +8,9 @@ function App() {
 
   return (
     <>
-      <MiApi />
       <Header />
       <Carrusel />
-      <Main />
+      <MiApi />
       <Footer />
     </>
   )
--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -1,12 +1,55 @@
-async function prueba(){
-  const data = await fetch("https://api-anime-production-0f6a.up.railway.app/getAllAnime?limit=10&page=1", {mode: "no-cors"});
-  const dataJson = await data.json();
-  console.log(dataJson)
-  return dataJson
-} 
+import { useState, useEffect } from "react";
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
-function MiApi(){
-  prueba()
-  return
+function MiApi() {
+    const [datosApi, setDatosApi] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
+    async function fetchData(){
+      try {
+        const data = await fetch("");
+        const dataJson = await data.json();
+        setDatosApi(dataJson); 
+      } catch (error) {
+        console.log(error)
+      }finally{
+        setLoading(false);
+      }
+    } 
+  
+    useEffect(()=>{
+      fetchData()
+    },[]);
+
+  return (
+    <>
+      {loading ? (
+      <h2>Loading...</h2>
+    ) : (
+      <main>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="https://i.pinimg.com/236x/49/5a/33/495a33e8f5f8113d54e77d1a3203fc7b.jpg" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroup.Item>Cras justo odio</ListGroup.Item>
+          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+          <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+        </ListGroup>
+        <Card.Body>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+    </main>
+    )}
+    </>
+  )
 }
 export default MiApi;
